ea295639a4d857dc65af5cf0395506b9c5c12dce
casimirth <timo.schlude@outlook.de>
2023-04-17 14:03:21 +0200

    added configuration for H2 base. Rest Mappings

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -1,18 +1,58 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class Controller {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
+    private final TodoItemRepository repository;
+
+    Controller(TodoItemRepository repository){
+        this.repository = repository;
+    }
+
 
     @GetMapping(value = "/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name){
         return new Greeting(counter.incrementAndGet(), String.format(template,name));
     }
+
+    @GetMapping(value = "/todos")
+    List<TodoItem> all(){
+        return repository.findAll();
+    }
+
+    @PostMapping("/todos")
+    TodoItem newTodoItem(@RequestBody TodoItem newTodoItem){
+        return repository.save(newTodoItem);
+    }
+
+    @GetMapping("/todos/{id}")
+    Optional<TodoItem> one(@PathVariable Long id){
+        return repository.findById(id);
+    }
+
+    @PutMapping("/todos/{id}")
+    TodoItem replaceTodoItem(@RequestBody TodoItem newTodoItem, @PathVariable Long id){
+        return repository.findById(id)
+                .map(todoItem -> {
+                    todoItem.setText(newTodoItem.getText());
+                    return repository.save(todoItem);
+                }).orElseGet(()->{
+                    newTodoItem.setText("");
+                    return repository.save(newTodoItem);
+                });
+    }
+
+    @DeleteMapping("/todos/{id}")
+    void deleteTodoItem(@PathVariable Long id){
+        repository.deleteById(id);
+    }
+
+
 }
--- src/main/java/com/example/demo/TodoItemRepository.java
+++ src/main/java/com/example/demo/TodoItemRepository.java
@@ -1,9 +1,11 @@
 package com.example.demo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
+//@RepositoryRestResource(collectionResourceRel = "rest", path="rest")
 public interface TodoItemRepository extends JpaRepository<TodoItem, Long> {
         List<TodoItem> findByTextStartsWithIgnoreCase(String text);
 }
